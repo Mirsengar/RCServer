8e88616ac4dabb6d1667a68c8037de34a6f8ef1f
mirsengar <mirsengar@gmail.com>
2022-06-04 19:42:25 +0430

    first commit

---
+++ Config.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MzQ;
+//اطلاعات اصلی برنامه که تغییر نمی کنند
+class Config
+{
+
+
+    const APPNAME       = "RCServer";
+    const DEBUG_MODE    = true;
+    const WEBSITE_URL       = "";
+
+
+    const servername    = "localhost";//میزبان
+    const username      = "";  //نام کاربری
+    const dbname        = "RCServer";  // نام دیتابیس
+    const password      = ""; // رمز عبور
+
+
+}
+
+
+
